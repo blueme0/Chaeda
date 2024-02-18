f0abee83aed7135be782dccbbfd0f0fa7680f915
blueme0 <ksytpdus0304@gmail.com>
2024-02-19 01:28:20 +0900

    [add/#5] 임시 클릭 리스너 추가

--- app/src/main/java/com/chaeda/chaeda/presentation/homework/HomeworkFragment.kt
+++ app/src/main/java/com/chaeda/chaeda/presentation/homework/HomeworkFragment.kt
@@ -1,23 +1,15 @@
 package com.chaeda.chaeda.presentation.homework
 
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.content.Intent
 import android.os.Bundle
-import android.os.Environment
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.chaeda.base.BindingFragment
 import com.chaeda.base.util.extension.setOnSingleClickListener
 import com.chaeda.chaeda.R
 import com.chaeda.chaeda.databinding.FragmentHomeworkBinding
+import com.chaeda.chaeda.presentation.homework.submit.ConfirmSubmitActivity
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
 
 @AndroidEntryPoint
 class HomeworkFragment
@@ -33,5 +25,8 @@ class HomeworkFragment
 
     private fun initListener() {
         // activity 시작
+        binding.tvPresign.setOnSingleClickListener {
+            startActivity(Intent(requireContext(), ConfirmSubmitActivity::class.java))
+        }
     }
 }
