19a33cc889ea996ba095302ea6af972a35833592
blueme0 <ksytpdus0304@gmail.com>
2024-02-22 23:31:14 +0900

    [feat/#8] 숙제 홈 화면 구현

--- app/src/main/java/com/chaeda/chaeda/presentation/homework/HomeworkFragment.kt
+++ app/src/main/java/com/chaeda/chaeda/presentation/homework/HomeworkFragment.kt
@@ -7,7 +7,9 @@ import androidx.viewpager2.widget.ViewPager2
 import com.chaeda.base.BindingFragment
 import com.chaeda.chaeda.R
 import com.chaeda.chaeda.databinding.FragmentHomeworkBinding
+import com.chaeda.chaeda.presentation.home.TodayHomeworkAdapter
 import com.chaeda.chaeda.presentation.homework.calendar.WeekFragmentStateAdapter
+import com.chaeda.domain.entity.Homework
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,12 +17,15 @@ class HomeworkFragment
     : BindingFragment<FragmentHomeworkBinding>(R.layout.fragment_homework) {
 
     private val viewModel by activityViewModels<HomeworkViewModel>()
+    private lateinit var homeworkAdapter: TodayHomeworkAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initListener()
         initViewPager()
+        initView()
+        initHomeworkItems()
     }
 
     private fun initListener() {
@@ -30,6 +35,24 @@ class HomeworkFragment
 //        }
     }
 
+    private fun initView() {
+        homeworkAdapter = TodayHomeworkAdapter {
+            // click listener
+        }
+
+        binding.rvHomework.adapter = homeworkAdapter
+    }
+
+    private fun initHomeworkItems() {
+        homeworkAdapter.setItems(
+            listOf(
+                Homework(HOMEWORK_TITLE, HOMEWORK_CONTENT, listOf("")),
+                Homework(HOMEWORK_TITLE, HOMEWORK_CONTENT, listOf("")),
+                Homework(HOMEWORK_TITLE, HOMEWORK_CONTENT, listOf(""))
+            )
+        )
+    }
+
     private fun initViewPager() {
         val fragmentStateAdapter = WeekFragmentStateAdapter(requireActivity())
         binding.vp.adapter = fragmentStateAdapter
@@ -37,5 +60,10 @@ class HomeworkFragment
         fragmentStateAdapter.apply {
             binding.vp.setCurrentItem(this.firstFragmentPosition, false)
         }
+    }
+
+    companion object {
+        private const val HOMEWORK_TITLE = "문제집 이름"
+        private const val HOMEWORK_CONTENT = "선생님이 작성한 본문 내용이 여기에 표시됩니다."
     }
 }
