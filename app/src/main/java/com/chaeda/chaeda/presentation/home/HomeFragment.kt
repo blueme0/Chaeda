0cd216f56ecf016c735cf6a452d1d9e99ef49b6e
blueme0 <ksytpdus0304@gmail.com>
2024-02-22 16:10:42 +0900

    [feat/#3] TodayHomework 리사이클러뷰

--- app/src/main/java/com/chaeda/chaeda/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/chaeda/chaeda/presentation/home/HomeFragment.kt
@@ -1,20 +1,46 @@
 package com.chaeda.chaeda.presentation.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.chaeda.base.BindingFragment
 import com.chaeda.chaeda.R
 import com.chaeda.chaeda.databinding.FragmentHomeBinding
+import com.chaeda.domain.entity.Homework
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment
     : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
+    private lateinit var homeworkAdapter: TodayHomeworkAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        initHomeworkItems()
+    }
+
+    private fun initView() {
+        homeworkAdapter = TodayHomeworkAdapter {
+            // click listener
+        }
+
+        binding.rvHomeToday.adapter = homeworkAdapter
+    }
+
+    private fun initHomeworkItems() {
+        homeworkAdapter.setItems(
+            listOf(
+                Homework(HOMEWORK_TITLE, HOMEWORK_CONTENT, listOf("")),
+                Homework(HOMEWORK_TITLE, HOMEWORK_CONTENT, listOf("")),
+                Homework(HOMEWORK_TITLE, HOMEWORK_CONTENT, listOf(""))
+            )
+        )
+    }
+
+    companion object {
+        private const val HOMEWORK_TITLE = "문제집 이름"
+        private const val HOMEWORK_CONTENT = "선생님이 작성한 본문 내용이 여기에 표시됩니다."
     }
 }
---
+++ app/src/main/java/com/chaeda/chaeda/presentation/home/TodayHomeworkAdapter.kt
@@ -0,0 +1,58 @@
+package com.chaeda.chaeda.presentation.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.chaeda.base.util.extension.setOnSingleClickListener
+import com.chaeda.chaeda.databinding.ItemHomeHomeworkBinding
+import com.chaeda.domain.entity.Homework
+
+class TodayHomeworkAdapter(private val itemClick: (Homework) -> (Unit))
+    : RecyclerView.Adapter<TodayHomeworkAdapter.TodayHomeworkViewHolder>() {
+
+    private val homeworkList = mutableListOf<Homework>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TodayHomeworkViewHolder {
+        val binding = ItemHomeHomeworkBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return TodayHomeworkViewHolder(binding,itemClick)
+    }
+
+    override fun onBindViewHolder(holder: TodayHomeworkViewHolder, position: Int) {
+        holder.onBind(homeworkList[position])
+    }
+
+    override fun getItemCount(): Int = homeworkList.size
+
+    fun setItems(newItems: List<Homework>) {
+        homeworkList.clear()
+        homeworkList.addAll(newItems)
+        notifyDataSetChanged()
+    }
+
+    class TodayHomeworkViewHolder(
+        private val binding: ItemHomeHomeworkBinding,
+        private val itemClick: (Homework) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun onBind(item: Homework) {
+            binding.tvTitle.text = item.title
+            binding.tvContent.text = item.content
+
+            binding.ivThumbnail.load(item.photoUrls.first()) {
+//                transformations(RoundedCornersTransformation(4f))
+            }
+
+            binding.root.setOnSingleClickListener {
+                itemClick(item)
+            }
+        }
+    }
+}
