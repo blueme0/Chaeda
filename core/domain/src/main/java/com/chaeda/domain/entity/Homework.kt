c6d948710b38993ded451c8b3699e7b995b58978
blueme0 <ksytpdus0304@gmail.com>
2024-02-22 17:00:38 +0900

    [fix/#8] Homework entity 수정

--- core/domain/src/main/java/com/chaeda/domain/entity/Homework.kt
+++ core/domain/src/main/java/com/chaeda/domain/entity/Homework.kt
@@ -3,5 +3,8 @@ package com.chaeda.domain.entity
 data class Homework(
     val title: String,
     val content: String,
-    val photoUrls: List<String>
+    val photoUrls: List<String>,
+    val range: String = "79p - 81p",
+    val deadline: String = "2024-02-22T15:30:45",
+    val isDone: Boolean = false
 )
