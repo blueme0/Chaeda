ada647b62c1c1ac00c8a9a21bc3e9e74aab1e5cf
blueme0 <ksytpdus0304@gmail.com>
2024-02-23 15:20:35 +0900

    [feat/#8] 숙제 결과 확인 - 탭 레이아웃 연결

---
+++ app/src/main/java/com/chaeda/chaeda/presentation/homework/result/classmate/ResultClassmateFragment.kt
@@ -0,0 +1,15 @@
+package com.chaeda.chaeda.presentation.homework.result.classmate
+
+import android.os.Bundle
+import android.view.View
+import com.chaeda.base.BindingFragment
+import com.chaeda.chaeda.R
+import com.chaeda.chaeda.databinding.FragmentResultClassmateBinding
+
+class ResultClassmateFragment
+    : BindingFragment<FragmentResultClassmateBinding>(R.layout.fragment_result_classmate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/chaeda/chaeda/presentation/homework/result/photo/ResultPhotoFragment.kt
@@ -0,0 +1,15 @@
+package com.chaeda.chaeda.presentation.homework.result.photo
+
+import android.os.Bundle
+import android.view.View
+import com.chaeda.base.BindingFragment
+import com.chaeda.chaeda.R
+import com.chaeda.chaeda.databinding.FragmentResultPhotoBinding
+
+class ResultPhotoFragment
+    : BindingFragment<FragmentResultPhotoBinding>(R.layout.fragment_result_photo) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
