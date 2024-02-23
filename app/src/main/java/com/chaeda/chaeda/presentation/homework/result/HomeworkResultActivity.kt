7d777149afae96e95d64dadbbb3570ed163bd058
blueme0 <ksytpdus0304@gmail.com>
2024-02-23 14:25:21 +0900

    [feat/#8] 숙제 결과 확인 activity

---
+++ app/src/main/java/com/chaeda/chaeda/presentation/homework/result/HomeworkResultActivity.kt
@@ -0,0 +1,16 @@
+package com.chaeda.chaeda.presentation.homework.result
+
+import android.os.Bundle
+import com.chaeda.base.BindingActivity
+import com.chaeda.chaeda.R
+import com.chaeda.chaeda.databinding.ActivityHomeworkResultBinding
+
+class HomeworkResultActivity
+    : BindingActivity<ActivityHomeworkResultBinding>(R.layout.activity_homework_result) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
