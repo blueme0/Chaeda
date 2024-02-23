6e92eb093b97314b562c92c8a642ad1d4e267476
blueme0 <ksytpdus0304@gmail.com>
2024-02-24 01:43:40 +0900

    [feat/#15] 공지 목록 구현

---
+++ app/src/main/java/com/chaeda/chaeda/presentation/notice/NoticeListActivity.kt
@@ -0,0 +1,63 @@
+package com.chaeda.chaeda.presentation.notice
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.chaeda.base.BindingActivity
+import com.chaeda.base.util.extension.setOnSingleClickListener
+import com.chaeda.chaeda.R
+import com.chaeda.chaeda.databinding.ActivityNoticeListBinding
+import com.chaeda.chaeda.presentation.home.RecentNoticeAdapter
+import com.chaeda.domain.entity.Notice
+import timber.log.Timber
+
+class NoticeListActivity
+    : BindingActivity<ActivityNoticeListBinding>(R.layout.activity_notice_list) {
+
+    private lateinit var noticeAdapter: RecentNoticeAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initView()
+        initNoticeItems()
+        initListener()
+    }
+
+    private fun initView() {
+        noticeAdapter = RecentNoticeAdapter {
+            // click listener
+        }
+
+        binding.rvNotice.adapter = noticeAdapter
+    }
+
+    private fun initListener() {
+        with(binding) {
+            ivBack.setOnSingleClickListener { finish() }
+        }
+    }
+
+    private fun initNoticeItems() {
+        Timber.tag("chaeda-items").d("initNoticeItems called")
+        noticeAdapter.setItems(
+            listOf(
+                Notice("전체", "학원 전체 공지사항", "2024-02-23T15:30:45"),
+                Notice("전체", "학원 전체 공지사항", "2024-02-22T15:30:45"),
+                Notice("전체", "학원 전체 공지사항", "2024-02-21T15:30:45"),
+                Notice("전체", "학원 전체 공지사항", "2024-02-20T15:30:45"),
+                Notice("전체", "학원 전체 공지사항", "2024-02-19T15:30:45"),
+                Notice("전체", "학원 전체 공지사항", "2024-02-18T15:30:45"),
+                Notice("전체", "학원 전체 공지사항", "2024-02-17T15:30:45"),
+                Notice("전체", "학원 전체 공지사항", "2024-02-16T15:30:45"),
+                Notice("전체", "학원 전체 공지사항", "2024-02-15T15:30:45"),
+                Notice("전체", "학원 전체 공지사항", "2024-02-14T15:30:45")
+            )
+        )
+        Timber.tag("chaeda-items").d("initNoticeItems : ${noticeAdapter.itemCount}")
+    }
+
+    companion object {
+        fun getIntent(context: Context) = Intent(context, NoticeListActivity::class.java)
+    }
+}
