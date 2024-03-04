73139eb21b049bf09d05cd0b267c9f33220b8762
blueme0 <ksytpdus0304@gmail.com>
2024-03-04 15:09:36 +0900

    [refactor/#20] repository impl 이동

--- core/data/src/main/java/com/chaeda/data/repository/HomeworkRepositoryImpl.kt
+++ core/data/src/main/java/com/chaeda/data/repository/HomeworkRepositoryImpl.kt
@@ -4,7 +4,8 @@ import com.chaeda.data.datasoure.remote.RemoteSampleDataSource
 import com.chaeda.domain.repository.HomeworkRepository
 import javax.inject.Inject
 
-class HomeworkRepositoryImpl @Inject constructor(private val remoteSampleDataSource: RemoteSampleDataSource)
-    : HomeworkRepository {
+class HomeworkRepositoryImpl @Inject constructor(
+    private val remoteSampleDataSource: RemoteSampleDataSource
+) : HomeworkRepository {
 
 }
--- core/domain/src/main/java/com/chaeda/domain/repository/ImageRepository.kt
+++ core/domain/src/main/java/com/chaeda/domain/repository/ImageRepository.kt
@@ -1,6 +1,14 @@
 package com.chaeda.domain.repository
 
+import com.chaeda.domain.entity.FileWithName
+import com.chaeda.domain.entity.ImageInfo
+import com.chaeda.domain.entity.PresignedInfo
+import java.io.File
+
 interface ImageRepository {
-
-
+    suspend fun getPresignedUrl(memberId: Int, imageType: String, imageFileExtension: String): Result<PresignedInfo>
+    suspend fun putFileToUrl(url: String, contentType: String, file: File): Result<String>
+    suspend fun uploadImages(images: List<FileWithName>): Result<String>
+    suspend fun noticePresignedUrl(memberId: Int, imageType: String, imageFileExtension: String, imageKey: String): Result<Any>
+    suspend fun getImagesUrl(memberId: Int, images: List<ImageInfo>): Result<List<String>>
 }
