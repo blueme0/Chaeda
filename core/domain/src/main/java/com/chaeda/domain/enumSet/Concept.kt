df5a25a23c745c98fe83ea9a00bfa4ab97afc84b
blueme0 <ksytpdus0304@gmail.com>
2024-05-17 19:50:56 +0900

    [fix/#39] enum 수정

--- core/domain/src/main/java/com/chaeda/domain/enumSet/Concept.kt
+++ core/domain/src/main/java/com/chaeda/domain/enumSet/Concept.kt
@@ -1,114 +1,114 @@
 package com.chaeda.domain.enumSet
 
-enum class Concept(val englishName: String, val koreanName: String) {
+enum class Concept(val chapter: String, val koreanName: String) {
     /*------------------수학 상-----------------------*/
     // Polynomial : 다항식
-    Operations_of_polynomials("Operations_of_polynomials", "다항식의 연산"),
-    Remainder_theorem_and_factorization("Remainder_theorem_and_factorization", "나머지정리와 인수분해"),
+    Operations_of_polynomials(Chapter.Polynomial.name, "다항식의 연산"),
+    Remainder_theorem_and_factorization(Chapter.Polynomial.name,"나머지정리와 인수분해"),
 
     // Equations : 방정식
-    Complex_numbers("Complex_numbers", "복소수"),
-    Quadratic_equations("Quadratic_equations", "이차방정식"),
-    Quadratic_equations_and_quadratic_functions("Quadratic_equations_and_quadratic_functions", "이차방정식과 이차함수"),
-    Various_types_of_equations("Various_types_of_equations", "여러 가지 방정식"),
+    Complex_numbers(Chapter.Equations.name, "복소수"),
+    Quadratic_equations(Chapter.Equations.name,  "이차방정식"),
+    Quadratic_equations_and_quadratic_functions(Chapter.Equations.name,  "이차방정식과 이차함수"),
+    Various_types_of_equations(Chapter.Equations.name, "여러 가지 방정식"),
 
     // Inequalities : 부등식
-    Linear_inequalities("Linear_inequalities", "일차부등식"),
-    Quadratic_inequalities("Quadratic_inequalities", "이차부등식"),
+    Linear_inequalities(Chapter.Inequalities.name, "일차부등식"),
+    Quadratic_inequalities(Chapter.Inequalities.name, "이차부등식"),
 
     // Equations_of_Shapes : 도형의 방정식
-    Plane_coordinates("Plane_coordinates", "평면좌표"),
-    Equations_of_lines("Equations_of_lines", "직선의 방정식"),
-    Equations_of_circles("Equations_of_circles", "원의 방정식"),
-    Transformation_of_shapes("Transformation_of_shapes", "도형의 이동"),
+    Plane_coordinates(Chapter.Equations_of_Shapes.name, "평면좌표"),
+    Equations_of_lines(Chapter.Equations_of_Shapes.name, "직선의 방정식"),
+    Equations_of_circles(Chapter.Equations_of_Shapes.name, "원의 방정식"),
+    Transformation_of_shapes(Chapter.Equations_of_Shapes.name, "도형의 이동"),
 
 
     /*------------------수학 하-----------------------*/
     // Sets_and_Propositions : 집합과 명제
-    Meaning_and_representation_of_sets("Meaning_and_representation_of_sets", "집합의 뜻과 표현"),
-    Operations_of_sets("Operations_of_sets", "집합의 연산"),
-    Propositions("Propositions", "명제"),
+    Meaning_and_representation_of_sets(Chapter.Sets_and_Propositions.name, "집합의 뜻과 표현"),
+    Operations_of_sets(Chapter.Sets_and_Propositions.name, "집합의 연산"),
+    Propositions(Chapter.Sets_and_Propositions.name, "명제"),
 
     // Functions : 함수
-    Functions("Functions", "함수"),
-    Rational_expressions_and_rational_functions("Rational_expressions_and_rational_functions", "유리식과 유리함수"),
-    Irrational_expressions_and_irrational_functions("Irrational_expressions_and_irrational_functions", "무리식과 무리함수"),
+    Functions(Chapter.Functions.name, "함수"),
+    Rational_expressions_and_rational_functions(Chapter.Functions.name, "유리식과 유리함수"),
+    Irrational_expressions_and_irrational_functions(Chapter.Functions.name, "무리식과 무리함수"),
 
     // Permutations_and_Combinations : 순열과 조합
-    Permutations_and_combinations("Permutations_and_combinations", "순열과 조합"),
+    Permutations_and_combinations(Chapter.Permutations_and_Combinations.name, "순열과 조합"),
 
 
     /*------------------수1-----------------------*/
     //Exponential_and_Logarithmic_Functions : 지수함수와 로그함수
-    Exponents_and_Logarithms("Exponents_and_Logarithms", "지수와 로그"),
-    Exponential_and_Logarithmic_Functions("Exponential_and_Logarithmic_Functions", "지수함수와 로그함수"),
+    Exponents_and_Logarithms(Chapter.Exponential_and_Logarithmic_Functions.name, "지수와 로그"),
+    Exponential_and_Logarithmic_Functions(Chapter.Exponential_and_Logarithmic_Functions.name, "지수함수와 로그함수"),
 
     //Trigonometric_Functions : 삼각함수
-    Trigonometric_Functions("Trigonometric_Functions", "삼각함수"),
-    Graphs_of_Trigonometric_Functions("Graphs_of_Trigonometric_Functions", "삼각함수의 그래프"),
-    Applications_of_Trigonometric_Functions("Applications_of_Trigonometric_Functions", "삼각함수의 활용"),
+    Trigonometric_Functions(Chapter.Trigonometric_Functions.name, "삼각함수"),
+    Graphs_of_Trigonometric_Functions(Chapter.Trigonometric_Functions.name, "삼각함수의 그래프"),
+    Applications_of_Trigonometric_Functions(Chapter.Trigonometric_Functions.name, "삼각함수의 활용"),
 
     //Sequences : 수열
-    Arithmetic_and_Geometric_Sequences("Arithmetic_and_Geometric_Sequences", "등차수열과 등비수열"),
-    Sum_of_Sequences("Sum_of_Sequences", "수열의 합"),
-    Mathematical_Induction("Mathematical_Induction", "수학적 귀납법"),
+    Arithmetic_and_Geometric_Sequences(Chapter.Sequences.name, "등차수열과 등비수열"),
+    Sum_of_Sequences(Chapter.Sequences.name, "수열의 합"),
+    Mathematical_Induction(Chapter.Sequences.name, "수학적 귀납법"),
 
 
     /*------------------수학 II-----------------------*/
     // Functions_Limits_and_Continuity : 함수의 극한과 연속
-    Functions_Limits("Functions_Limits", "함수의 극한"),
-    Functions_Continuity("Functions_Continuity", "함수의 연속"),
+    Functions_Limits(Chapter.Functions_Limits_and_Continuity.name, "함수의 극한"),
+    Functions_Continuity(Chapter.Functions_Limits_and_Continuity.name, "함수의 연속"),
 
     // Differentiation : 미분
-    Differential_Coefficient("Differential_Coefficient", "미분계수"),
-    Derivative_Functions("Derivative_Functions", "도함수"),
-    Applications_of_Derivatives("Applications_of_Derivatives", "도함수의 활용"),
+    Differential_Coefficient(Chapter.Differentiation.name, "미분계수"),
+    Derivative_Functions(Chapter.Differentiation.name, "도함수"),
+    Applications_of_Derivatives(Chapter.Differentiation.name, "도함수의 활용"),
 
     // Integration : 적분
-    Indefinite_Integrals("Indefinite_Integrals", "부정적분"),
-    Definite_Integrals("Definite_Integrals", "정적분"),
-    Applications_of_Definite_Integrals_in_Math_2("Applications_of_Definite_Integrals_in_Math_2", "정적분의 활용"),
+    Indefinite_Integrals(Chapter.Integration_in_Math_2.name, "부정적분"),
+    Definite_Integrals(Chapter.Integration_in_Math_2.name, "정적분"),
+    Applications_of_Definite_Integrals_in_Math_2(Chapter.Integration_in_Math_2.name, "정적분의 활용"),
 
 
     /*------------------미적분-----------------------*/
     // Limits_of_Sequences : 수열의 극한
-    Limits_of_Sequences("Limits_of_Sequences", "수열의 극한"),
-    Series("Series", "급수"),
+    Limits_of_Sequences(Chapter.Limits_of_Sequences.name, "수열의 극한"),
+    Series(Chapter.Limits_of_Sequences.name, "급수"),
 
     // Differential_Calculus : 미분법
-    Differentiation_of_Various_Functions("Differentiation_of_Various_Functions", "여러 가지 함수의 미분"),
-    Various_Differentiation_Methods("Various_Differentiation_Methods", "여러 가지 미분법"),
-    Applications_of_Derivative_Functions("Applications_of_Derivative_Functions", "도함수의 활용"),
+    Differentiation_of_Various_Functions(Chapter.Differential_Calculus.name, "여러 가지 함수의 미분"),
+    Various_Differentiation_Methods(Chapter.Differential_Calculus.name, "여러 가지 미분법"),
+    Applications_of_Derivative_Functions(Chapter.Differential_Calculus.name, "도함수의 활용"),
 
     // Integration : 적분법
-    Various_Integration_Methods("Various_Integration_Methods", "여러 가지 적분법"),
-    Applications_of_Definite_Integrals_in_Calculus("Applications_of_Definite_Integrals_in_Calculus", "정적분의 활용"),
+    Various_Integration_Methods(Chapter.Integration_in_calculus.name, "여러 가지 적분법"),
+    Applications_of_Definite_Integrals_in_Calculus(Chapter.Integration_in_calculus.name, "정적분의 활용"),
 
 
     /*------------------확률과 통계-----------------------*/
     // Counting_Methods : 경우의 수
-    Permutations_and_Combinations("Permutations_and_Combinations", "순열과 조합"),
-    Binomial_Theorem("Binomial_Theorem", "이항정리"),
+    Permutations_and_Combinations(Chapter.Counting_Methods.name, "순열과 조합"),
+    Binomial_Theorem(Chapter.Counting_Methods.name, "이항정리"),
 
     // Probability : 확률
-    Probability("Probability", "확률"),
-    Conditional_Probability("Conditional_Probability", "조건부확률"),
+    Probability(Chapter.Probability.name, "확률"),
+    Conditional_Probability(Chapter.Probability.name, "조건부확률"),
 
     // Statistics : 통계
-    Probability_Distributions("Probability_Distributions", "확률분포"),
-    Statistical_Estimation("Statistical_Estimation", "통계적 추정"),
+    Probability_Distributions(Chapter.Statistics.name, "확률분포"),
+    Statistical_Estimation(Chapter.Statistics.name, "통계적 추정"),
 
 
     /*------------------기하-----------------------*/
     // Conic_Sections : 이차곡선
-    Conic_Sections("Conic_Sections", "이차곡선"),
-    Conic_Sections_and_Lines("Conic_Sections_and_Lines", "이차곡선과 직선"),
+    Conic_Sections(Chapter.Conic_Sections.name, "이차곡선"),
+    Conic_Sections_and_Lines(Chapter.Conic_Sections.name, "이차곡선과 직선"),
 
     // Plane_Vectors : 평면벡터
-    Vector_Operations("Vector_Operations", "벡터의 연산"),
-    Components_and_Dot_Product_of_Plane_Vectors("Components_and_Dot_Product_of_Plane_Vectors", "평면벡터의 성분과 내적"),
+    Vector_Operations(Chapter.Plane_Vectors.name, "벡터의 연산"),
+    Components_and_Dot_Product_of_Plane_Vectors(Chapter.Plane_Vectors.name, "평면벡터의 성분과 내적"),
 
     // Spatial_Shapes_and_Coordinates : 공간도형과 공간좌표
-    Spatial_Shapes("Spatial_Shapes", "공간도형"),
-    Spatial_Coordinates("Spatial_Coordinates", "공간좌표")
+    Spatial_Shapes(Chapter.Spatial_Shapes_and_Coordinates.name, "공간도형"),
+    Spatial_Coordinates(Chapter.Spatial_Shapes_and_Coordinates.name, "공간좌표")
 }
--- core/domain/src/main/java/com/chaeda/domain/enumSet/Subject.kt
+++ core/domain/src/main/java/com/chaeda/domain/enumSet/Subject.kt
@@ -1,41 +1,41 @@
 package com.chaeda.domain.enumSet
 
-enum class Subject(val englishName: String, val koreanName: String, val chapters: List<Chapter>) {
-    Math_high("Math_high", "수학 상", listOf(
+enum class Subject(val koreanName: String, val chapters: List<Chapter>) {
+    Math_high("수학 상", listOf(
         Chapter.Polynomial,
         Chapter.Equations,
         Chapter.Inequalities,
         Chapter.Equations_of_Shapes
     )),
-    Math_low("Math_low", "수학 하", listOf(
+    Math_low("수학 하", listOf(
         Chapter.Sets_and_Propositions,
         Chapter.Functions,
         Chapter.Permutations_and_Combinations
     )),
-    Math_1("Math_1", "수학1", listOf(
+    Math_1("수학1", listOf(
         Chapter.Exponential_and_Logarithmic_Functions,
         Chapter.Trigonometric_Functions,
         Chapter.Sequences
     )),
-    Math_2("Math_2", "수학2", listOf(
+    Math_2("수학2", listOf(
         Chapter.Functions_Limits_and_Continuity,
         Chapter.Differentiation,
         Chapter.Integration_in_Math_2
     )),
-    Calculus("Calculus", "미적분", listOf(
+    Calculus("미적분", listOf(
         Chapter.Limits_of_Sequences,
         Chapter.Differential_Calculus,
         Chapter.Integration_in_calculus
     )),
-    Probability_and_Statistics("Probability_and_Statistics", "확률과 통계", listOf(
+    Probability_and_Statistics("확률과 통계", listOf(
         Chapter.Counting_Methods,
         Chapter.Probability,
         Chapter.Statistics
     )),
-    Geometry("Geometry", "기하", listOf(
+    Geometry("기하", listOf(
         Chapter.Conic_Sections,
         Chapter.Plane_Vectors,
         Chapter.Spatial_Shapes_and_Coordinates
     )),
-    Mix("Mix", "혼합형", listOf())
+    Mix("혼합형", listOf())
 }
